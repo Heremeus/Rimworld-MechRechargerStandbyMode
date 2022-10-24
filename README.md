2a1d61b510f0a86cfd5ecbdec7949562edf518fd
Willer, Armin <awiller.ext@entwicklunggema.local>
2022-10-24 15:10:00 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rimworld - Mech Recharger Standby Mode
+Mech Recharger Standby Mode is a mod for the game Rimworld. The mod makes mech rechargers go on standby and draw less power when not in use.
+
+# Compatibility
+There are no known incompatibilities with other mods. Please let me know if you encounter any issues.
+### Save game compatibility
+This mod can be added and removed to existing saves at any time.
+
+# Credits
+* Implementation by Heremeus
---
+++ Source/RechargerStandby/HarmonyPatches.cs
@@ -0,0 +1,90 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+using Verse.AI;
+
+namespace RechargerStandby
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        // this static constructor runs to create a HarmonyInstance and install a patch.
+        static HarmonyPatches()
+        {
+            Patch();
+            Log.Message("Mech Recharger Standby Mode was loaded successfully");
+        }
+
+        public static void Patch()
+        {
+            Harmony harmony = new Harmony("heremeus.rimworld.rechargerstandby.main");
+
+            // find the MakeRecipeProducts method of the class GenRecipe
+            MethodInfo targetMethod = AccessTools.Method(typeof(Building_MechCharger), "StartCharging");
+            // find the static method to call after (i.e. postfix) the targetmethod
+            HarmonyMethod postfixmethod = new HarmonyMethod(typeof(HarmonyPatches).GetMethod("Building_MechCharger_StartOrStopCharging_Postfix"));
+            // patch the targetmethod, by calling postfixmethod after it ran, with no prefixmethod (i.e. null)
+            harmony.Patch(targetMethod, null, postfixmethod);
+
+            targetMethod = AccessTools.Method(typeof(Building_MechCharger), "StopCharging");
+            harmony.Patch(targetMethod, null, postfixmethod); // Patch with same postfix method
+
+            targetMethod = AccessTools.Method(typeof(CompPowerTrader), "SetUpPowerVars");
+            postfixmethod = new HarmonyMethod(typeof(HarmonyPatches).GetMethod("CompPowerTrader_SetUpPowerVars_Postfix"));
+            harmony.Patch(targetMethod, null, postfixmethod);
+        }
+
+        public static void Building_MechCharger_StartOrStopCharging_Postfix(Building_MechCharger __instance, Pawn ___currentlyChargingMech)
+        {
+            UpdatePowerConsumptionOfCharger(__instance, ___currentlyChargingMech);
+        }
+
+        public static void CompPowerTrader_SetUpPowerVars_Postfix(CompPowerTrader __instance)
+        {
+            Log.Message("Entered method");
+            if (__instance.parent == null) return;
+
+            Log.Message("Getting charger");
+            Building_MechCharger charger = __instance.parent as Building_MechCharger;
+            if (charger == null) return;
+
+            Log.Message("Getting field");
+            FieldInfo field = charger.GetType().GetField("currentlyChargingMech", BindingFlags.NonPublic | BindingFlags.Instance);
+            Log.Message("Getting Pawn from field " + field);
+            Pawn currentlyChargingMech = field.GetValue(charger) as Pawn;
+
+            Log.Message("Updating power");
+            UpdatePowerConsumptionOfCharger(charger, currentlyChargingMech);
+        }
+
+        private static void UpdatePowerConsumptionOfCharger(Building_MechCharger charger, Pawn currentlyChargingMech)
+        {
+            CompPowerTrader compPowerTrader = charger.GetComp<CompPowerTrader>();
+            if (compPowerTrader == null)
+            {
+                Log.Warning("Missing CompPowerTrader on mech charger");
+                return;
+            }
+            bool isCharging = currentlyChargingMech != null;
+            float powerConsumption;
+            if (isCharging)
+            {
+                powerConsumption = compPowerTrader.Props.PowerConsumption;
+            }
+            else
+            {
+                StandbyConsumptionDefModExtension defExtension = charger.def.GetModExtension<StandbyConsumptionDefModExtension>();
+                if (defExtension == null)
+                {
+                    Log.Warning("Missing StandbyConsumptionDefModExtension on mech charger def '" + charger.def.defName + "'");
+                    return;
+                }
+                powerConsumption = defExtension.standbyPowerConsumption;
+            }
+            compPowerTrader.PowerOutput = -1f * powerConsumption;
+        }
+    }
+}
